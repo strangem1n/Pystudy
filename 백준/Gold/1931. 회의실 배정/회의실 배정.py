fa96783c2ed6051f6a30b6f8a7300e7ea9cda9e9
strangem1n <strangem111n@gmail.com>
2025-03-17 17:17:46 +0900

    [Gold V] Title: 회의실 배정, Time: 2824 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr.sort(key=lambda x: (x[1], x[0]))
+result = 1
+start, end = arr[0]
+idx = 1
+while idx < n:
+    if end <= arr[idx][0]:
+        result += 1
+        start, end = arr[idx]
+    idx += 1
+print(result)
