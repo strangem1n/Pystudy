319d9aaf6ff50efe6a04e432831ea24e02eea746
strangem1n <strangem111n@gmail.com>
2025-03-19 11:57:20 +0900

    [D3] Title: 경로의 수, Time: 54 ms, Memory: 49,920 KB -BaekjoonHub

---
+++ SWEA/D3/24220. 경로의 수/README.md
@@ -0,0 +1,15 @@
+# [D3] 경로의 수 - 24220 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZWsDXh6C8fHBISP) 
+
+### 성능 요약
+
+메모리: 49,920 KB, 시간: 54 ms, 코드길이: 574 Bytes
+
+### 제출 일자
+
+2025-03-19 11:57
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/24220. 경로의 수/경로의 수.py
@@ -0,0 +1,21 @@
+def dfs(start, end):    # 재귀로 DFS 구현
+    global result
+    if start == end:    # 도착하면 경로 +1
+        result += 1
+    else:
+        for middle in adj[start]:    # 현 지점에서 갈 수 있는 곳 탐색
+            dfs(middle, end)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N, E = map(int, input().split())
+    arr = list(map(int, input().split()))
+    S, G = map(int, input().split())
+    adj = [[] for _ in range(N+1)]
+    for i in range(E):
+        adj[arr[i*2]].append(arr[i*2+1])    # 방향성 있는 그래프
+
+    result = 0
+    dfs(S, G)
+    print(f"#{tc} {result}")
