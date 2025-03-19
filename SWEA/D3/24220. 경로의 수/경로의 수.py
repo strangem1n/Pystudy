83304b31753112eb9698ce9ee1c9db29bd3c09c2
strangem1n <strangem111n@gmail.com>
2025-03-19 13:50:04 +0900

    [D3] Title: 경로의 수, Time: 54 ms, Memory: 49,920 KB -BaekjoonHub

--- SWEA/D3/24220. 경로의 수/경로의 수.py
+++ SWEA/D3/24220. 경로의 수/경로의 수.py
@@ -4,13 +4,17 @@ def dfs(start, end):    # 재귀로 DFS 구현
         result += 1
     else:
         for middle in adj[start]:    # 현 지점에서 갈 수 있는 곳 탐색
-            dfs(middle, end)
+            if visited[middle] == 0:
+                visited[middle] = 1
+                dfs(middle, end)
+                visited[middle] = 0
 
 
 T = int(input())
 for tc in range(1, T+1):
     N, E = map(int, input().split())
     arr = list(map(int, input().split()))
+    visited = [0] * (N+1)
     S, G = map(int, input().split())
     adj = [[] for _ in range(N+1)]
     for i in range(E):
