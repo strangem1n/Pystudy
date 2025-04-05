acd7328f2d27a649362a7eb7d3d0ae7c24e8135f
strangem1n <strangem111n@gmail.com>
2025-04-06 01:46:41 +0900

    [Silver I] Title: IOIOI, Time: 280 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,10 +1,26 @@
 n = int(input())
 m = int(input())
-string = input()
+string = input().rstrip() + "E"
 
-chk = "I" + ("OI" * n)
-cnt = 0
-for i in range(m-2*n):
-    if chk == string[i:i+(2*n+1)]:
-        cnt += 1
-print(cnt)
+ioi = 0
+ioi_length = 2*n+1
+subset = 0
+result = 0
+for i in range(m+1):
+    if ioi != 2 and string[i] == "I":
+        if subset >= ioi_length:
+            result += (subset - ioi_length) // 2 + 1
+        ioi = 1
+        subset = 1
+    elif ioi == 1 and string[i] == "O":
+        ioi = 2
+        subset += 1
+    elif ioi == 2 and string[i] == "I":
+        ioi = 1
+        subset += 1
+    else:
+        if subset >= ioi_length:
+            result += (subset - ioi_length) // 2 + 1
+        ioi = 0
+        subset = 0
+print(result)
