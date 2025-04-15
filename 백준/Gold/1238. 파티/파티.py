eaf7522a0e352f5d6c447c59384fb0ab3859e757
strangem1n <strangem111n@gmail.com>
2025-04-15 12:45:35 +0900

    [Gold III] Title: 파티, Time: 52 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -2,32 +2,42 @@ import sys,heapq
 input = sys.stdin.readline
 
 
-def dijkstra(start, end):
+def dijkstra(direction):
     dist = [float('inf')] * (n+1)
     pq = []
-    heapq.heappush(pq, (0, start))
+    heapq.heappush(pq, (0, x))
     while pq:
         cost, node = heapq.heappop(pq)
         if dist[node] <= cost:
             continue
         dist[node] = cost
-        for next_cost, new_node in adj[node]:
-            new_cost = cost + next_cost
-            if dist[new_node] <= new_cost:
-                continue
-            heapq.heappush(pq, (new_cost, new_node))
-    return dist[end]
+        if direction == 1:
+            for next_cost, new_node in adj_for[node]:
+                new_cost = cost + next_cost
+                if dist[new_node] <= new_cost:
+                    continue
+                heapq.heappush(pq, (new_cost, new_node))
+        else:
+            for next_cost, new_node in adj_rev[node]:
+                new_cost = cost + next_cost
+                if dist[new_node] <= new_cost:
+                    continue
+                heapq.heappush(pq, (new_cost, new_node))
+    return dist
 
 
 n, m, x = map(int, input().split())
-adj = [[] for _ in range(n+1)]
+adj_for = [[] for _ in range(n+1)]
+adj_rev = [[] for _ in range(n+1)]
 for _ in range(m):
     a, b, c = map(int, input().split())
-    adj[a].append([c, b])
+    adj_for[a].append([c, b])
+    adj_rev[b].append([c, a])
+
 
 max_t = 0
+dist_for, dist_rev = dijkstra(1), dijkstra(0)
 for i in range(1, n+1):
-    t = dijkstra(i, x) + dijkstra(x, i)
-    if max_t < t:
-        max_t = t
+    if max_t < dist_for[i] + dist_rev[i]:
+        max_t = dist_for[i] + dist_rev[i]
 print(max_t)
