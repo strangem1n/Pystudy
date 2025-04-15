94b29076e1c9f1f2b97a642b7afaee32e4916329
strangem1n <strangem111n@gmail.com>
2025-04-15 13:01:28 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 116 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,36 @@
+import sys,heapq
+input = sys.stdin.readline
+
+
+def dijkstra(N):
+    pq = []
+    heapq.heappush(pq, (arr[0][0], 0, 0))
+    while pq:
+        cost, i, j = heapq.heappop(pq)
+        if dist[i][j] <= cost:
+            continue
+        dist[i][j] = cost
+        for k in range(4):
+            ni, nj = i + di[k], j + dj[k]
+            if 0 <= ni < n and 0 <= nj < n:
+                new_cost = cost + arr[ni][nj]
+                if dist[ni][nj] <= new_cost:
+                    continue
+                heapq.heappush(pq, (new_cost, ni, nj))
+    return dist[N-1][N-1]
+
+
+di = [1, 0, -1, 0]
+dj = [0, 1, 0, -1]
+cnt = 0
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    cnt += 1
+
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    dist = [[float('inf')] * n for _ in range(n)]
+    result = dijkstra(n)
+    print(f'Problem {cnt}: {result}')
