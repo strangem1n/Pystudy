fbdd3ec898e8571cb315bc40ab303cbdec30fbb8
strangem1n <strangem111n@gmail.com>
2025-04-22 10:46:38 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1520 ms, Memory: 281484 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -2,34 +2,38 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+di = [1, 0, -1, 0]
+dj = [0, 1, 0, -1]
+
+
+def bfs():
+    if n == m == 1:
+        return 1
+
+    q = deque([[0, 0, 1]])
+    while q:
+        i, j, b = q.popleft()
+        for k in range(4):
+            ni, nj = i + di[k], j + dj[k]
+            if 0 <= ni < n and 0 <= nj < m:
+                if arr[ni][nj] == 0:
+                    if ni == n-1 and nj == m-1:
+                        result = visited[i][j][0] + 1 if b == 1 else visited[i][j][1] + 1
+                        return result
+                    elif b == 1 and visited[ni][nj][0] == 0:
+                        visited[ni][nj][0] = visited[i][j][0] + 1
+                        q.append([ni, nj, b])
+                    elif b == 0 == visited[ni][nj][1]:
+                        visited[ni][nj][1] = visited[i][j][1] + 1
+                        q.append([ni, nj, b])
+                elif arr[ni][nj] == 1 == b:
+                    visited[ni][nj][1] = visited[i][j][0] + 1
+                    q.append([ni, nj, 0])
+    return -1
+
+
 n, m = map(int, input().split())
 arr = [list(map(int, input().rstrip())) for _ in range(n)]
 visited = [[[0, 0] for _ in range(m)] for __ in range(n)]
 visited[0][0] = [1, 1]
-q = deque([[0, 0, 1]])
-while q:
-    i, j, b = q.popleft()
-    for di, dj in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-        ni, nj = i + di, j + dj
-        if 0 <= ni < n and 0 <= nj < m:
-            if arr[ni][nj] == 0:
-                if b == 1 and visited[ni][nj][0] == 0:
-                    visited[ni][nj][0] = visited[i][j][0] + 1
-                    q.append([ni, nj, b])
-                elif b == 0 == visited[ni][nj][1]:
-                    visited[ni][nj][1] = visited[i][j][1] + 1
-                    q.append([ni, nj, b])
-            elif arr[ni][nj] == 1 == b:
-                visited[ni][nj][1] = visited[i][j][0] + 1
-                q.append([ni, nj, 0])
-
-result = visited[n-1][m-1]
-if result[0] == result[1] == 0:
-    print(-1)
-else:
-    if result[0] == 0:
-        print(result[1])
-    elif result[1] == 0:
-        print(result[0])
-    else:
-        print(min(result))
+print(bfs())
