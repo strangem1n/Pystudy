af68382a65d614cc175f8d7824c586f582179a21
strangem1n <strangem111n@gmail.com>
2025-05-18 00:38:44 +0900

    [Gold V] Title: 용액, Time: 128 ms, Memory: 46816 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,28 @@
+import sys, bisect
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+if arr[-1] <= 0:
+    print(arr[-2], arr[-1])
+elif arr[0] >= 0:
+    print(arr[0], arr[1])
+else:
+    a = b = 0
+    min_diff = float('inf')
+    for i in range(n):
+        res = bisect.bisect_left(arr, -arr[i])
+        if i < res:
+            res -= 1
+        if arr[i] == arr[res]:
+            continue
+        if min_diff > abs(arr[i] + arr[res]):
+            min_diff = abs(arr[i] + arr[res])
+            if arr[i] < arr[res]:
+                a, b = arr[i], arr[res]
+            else:
+                a, b = arr[res], arr[i]
+            if min_diff == 0:
+                break
+    print(a, b)
